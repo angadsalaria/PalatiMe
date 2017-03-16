b5f116604cc589ffca8f47e4a76622798311f6dd
Angad <angadsalaria@users.noreply.github.com>
2017-03-16 13:16:43 -0400

    Invalidated Auth0 JWT keys

--- Palati/src/main/java/com/palati/util/ConstUtil.java
+++ Palati/src/main/java/com/palati/util/ConstUtil.java
@@ -2,6 +2,7 @@ package com.palati.util;
 
 public class ConstUtil {
 	
+	// Below JWT params are no longer valid ( as of 16-MAR-2017 )
 	public static final String JWT_AUDIENCE = "t36ibpYH70xL1KyBEPAURJLI4sAJcPlG";
 	public static final String JWT_SECRET = "u7LsR2MwD-pKR8YLeGFI_zwLOrNqG0BwV__MiR4xGjNJLULwNfKPdXPg6oYYO7hf";
 
